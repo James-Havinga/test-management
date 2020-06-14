73ecfb9ee3eca6d71586788be62e56d64a82b296
JamesHavinga <180099@virtualwindow.co.za>
2020-06-14 17:45:37 +0200

    Add default roles and admin user

--- test-managment/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ test-managment/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -96,6 +96,7 @@ namespace test_managment.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
+                    _userManager.AddToRoleAsync(user, "Patient").Wait();
                     _logger.LogInformation("User created a new account with password.");
 
                     await _signInManager.SignInAsync(user, isPersistent: false);
--- test-managment/Startup.cs
+++ test-managment/Startup.cs
@@ -43,13 +43,20 @@ namespace test_managment
             services.AddAutoMapper(typeof(Maps));
 
             services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(
+            IApplicationBuilder app, 
+            IWebHostEnvironment env,
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager
+            )
         {
             if (env.IsDevelopment())
             {
@@ -69,6 +76,8 @@ namespace test_managment
 
             app.UseAuthentication();
             app.UseAuthorization();
+
+            SeedData.Seed(userManager, roleManager);
 
             app.UseEndpoints(endpoints =>
             {
