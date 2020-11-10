202a88d2fb63daf9e8bac05e764e4225764e6bf2
JamesHavinga <180099@virtualwindow.co.za>
2020-11-10 23:52:05 +0200

    created custom login and register forms

--- test-managment/Controllers/HomeController.cs
+++ test-managment/Controllers/HomeController.cs
@@ -6,9 +6,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using test_managment.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace test_managment.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
